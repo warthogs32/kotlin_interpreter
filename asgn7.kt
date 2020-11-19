81d5c1dba7ac626e8d8741a5bce1f1569867c6a4
warthogs32 <jj@kefi-foundation.org>
2020-11-19 11:07:57 -0800

    skeleton code

--- asgn7.kt
+++ asgn7.kt
@@ -1,10 +1,62 @@
 interface ExprC
 {
 }
+class StringC : ExprC
+{
+	var s: String
+}
 class NumC : ExprC
 {
+    var n: Double
 }
 class IfC : ExprC
 {
+    var a: ExprC
+    var b: ExprC
+    var c: ExprC
 }
+class IdC : ExprC
+{
+	var s: String
+}
+class AppC : ExprC
+{
+	var fn: ExprC 
+	var argmt: ArrayList<ExprC>
+}
+class LamC : ExprC
+{
+	var argmtx: ArrayList<String>
+	var body: ArrayList<ExprC>
+}
+
+interface Value
+{
+}
+class StringV : Value
+{
+	var s: String
+}
+class NumV : Value
+{
+    var n: Double
+}
+}
+class LamV : Value
+{
+	var argmtx: ArrayList<String>
+	var body: ArrayList<ExprC>
+}
+class CloV : Value
+{
+	var param: ArrayList<String>
+    var body: ExprC
+	var CloEnv: HashMap<String, Value>
+}
+class PrimV : Value
+{
+	var op: (Any)->Value
+}
+    
+
 
