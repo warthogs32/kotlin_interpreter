3b5bab7110f2bd5ba8a1fcc822b73623788ffebc
DESKTOP-GSNGBKD\jason <jj@jj-jj.org>
2020-11-20 12:21:21 -0800

    work on interp

--- asgn7.kt
+++ asgn7.kt
@@ -10,7 +10,7 @@ class IdC (var s: String) : ExprC {}
 
 class AppC (var fn: ExprC, var argmt: ArrayList<ExprC>) : ExprC {}
 
-class LamC(var arguement: ArrayList<String>, var body: ArrayList<ExprC>) : ExprC {}
+class LamC(var arguement: ArrayList<String>, var body: ExprC) : ExprC {}
 
 interface Value {}
 
@@ -27,6 +27,26 @@ class PrimV (var op: (Any)->Value) : Value {}
 class BoolV (var b: Boolean) : Value {}    
 
 class Env (var bindings: HashMap<String, Value>) {}
+
+fun interp(expression: ExprC, environment: Env): Value
+{
+	when (expression)
+	{
+		is NumC -> return NumV(expression.n)
+		is StringC -> return StringV(expression.s)
+		is IdC -> {
+					var idSymbol = environment.bindings.get(expression.s)
+					if (idSymbol != null) {return idSymbol} else {throw Exception("DXUQ: Unbound identifier: ~e")}
+		}
+		is LamC -> return CloV(expression.arguement, expression.body, environment.bindings)
+		is IfC -> {
+					var condition = interp(expression.a, environment)
+					if (condition is BoolV) 
+						{if (condition.b) {return interp(expression.b, environment)} else {return interp(expression.c, environment)}} else {throw Exception("DXUQ: IfC does not contain a conditional")}
+		}
+		else -> {throw Exception("interp DXUQ: bad input")}
+	}
+}
 
 fun main(args: Array<String>) 
 {
