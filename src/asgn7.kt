84ab8a050f29209614b92b4b5277038a55cb5e27
Ethan Waite <43476516+ethanbwaite@users.noreply.github.com>
2020-11-21 15:09:36 -0800

    finished

--- src/asgn7.kt
+++ src/asgn7.kt
@@ -7,7 +7,28 @@ var topEnv = Env(hashMapOf("+" to PrimV({a: Value, b: Value -> NumV((a as NumV).
 			"true" to BoolV(true),
 			"false" to BoolV(false)))
 
-fun parse(expression: String): ExprC {}
+fun parse(expression: Array<Any>): ExprC {
+	if (expression.size == 1 && expression[0] is String) { // single token base cases
+		if ((expression[0] as String).matches("-?\\d+(\\.\\d+)?".toRegex())) // token is a number
+			return NumC((expression[0] as String).toDouble())
+		if ((expression[0] as String).matches("'\\w*'".toRegex())) // token is a string
+			return StringC((expression[0] as String))
+		return IdC((expression[0] as String)) // else, token is an id
+	}
+
+	if (expression.size == 4 && (expression[0] as String) == "if") { // if case
+		return IfC(parse(arrayOf(expression[1])), parse(arrayOf(expression[2])), parse(arrayOf(expression[3])))
+	}
+
+	if (expression.isNotEmpty()) {
+		if (expression.size == 1 && expression[0] is Array<*>) {
+			var exp = (expression[0] as Array<Any>)
+			return AppC(parse(arrayOf(exp[0])), (exp.toList().subList(1, exp.size)).map { parse(arrayOf(it)) }.toTypedArray())
+		}
+		return AppC(parse(arrayOf(expression[0])), (expression.toList().subList(1,expression.size)).map{parse(arrayOf(it))}.toTypedArray())
+	}
+	throw Exception("DXUQ: Bad input")
+}
 
 fun interp(expression: ExprC, environment: Env): Value
 {
@@ -66,8 +87,18 @@ fun main()
 	println(((interp(AppC(IdC("equal?"), arrayOf<ExprC>(IdC("true"), IdC("true"))), topEnv)) as BoolV).b)
 	println(((interp(AppC(IdC("equal?"), arrayOf<ExprC>(IdC("true"), NumC(1.0))), topEnv)) as BoolV).b)*/
 
+	// Interp tests
 	println(serialize(interp(NumC(1.0), topEnv)))
 	println(serialize(interp(AppC(IdC("<="), arrayOf<ExprC>(NumC(1.0), NumC(2.0))), topEnv)))
-
 	println(serialize(interp(AppC(LamC(arrayOf("x", "y"), AppC(IdC("+"), arrayOf<ExprC>(IdC("x"), IdC("y")))), arrayOf<ExprC>(NumC(2.0), NumC(1.0))), topEnv)))
+
+	// Parse tests
+	println(parse(arrayOf("7.0")))
+	println(parse(arrayOf("id")))
+	println(parse(arrayOf("'string'")))
+	println(serialize(interp(parse(arrayOf("if", "true", "1", "2")), topEnv)))
+	println(serialize(interp(parse(arrayOf("if", "false", "1", "2")), topEnv)))
+	println(serialize(interp(parse(arrayOf("+", "1", "2")), topEnv)))
+	println(serialize(interp(parse(arrayOf("+", "1", arrayOf("+", "2", "3"))), topEnv)))
+	println(serialize(interp(parse(arrayOf("+", "1", arrayOf("+", "2", arrayOf("+", "3", "4")))), topEnv)))
 }
